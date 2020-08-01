411578cb4620093b2cc0813d6d268a88a5a1e428
Alia Arina <arina.aal98@gmail.com>
2020-08-01 12:28:04 +0800

    Updates

---
+++ precache-manifest.471ff0ce6c71f6c3fd56426fd23ebbfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4471f317f6dc3d4748b1a22442da59e1",
+    "url": "/react_todo_app/index.html"
+  },
+  {
+    "revision": "ee9b26e3cf06757163ed",
+    "url": "/react_todo_app/static/css/main.77d72a3a.chunk.css"
+  },
+  {
+    "revision": "57abbdbfad0deeca8390",
+    "url": "/react_todo_app/static/js/2.ac153c84.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react_todo_app/static/js/2.ac153c84.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ee9b26e3cf06757163ed",
+    "url": "/react_todo_app/static/js/main.326e77ed.chunk.js"
+  },
+  {
+    "revision": "18278ec60ab5ab30c83c",
+    "url": "/react_todo_app/static/js/runtime-main.d7891c1e.js"
+  }
+]);
